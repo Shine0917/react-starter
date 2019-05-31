f0c57863ed81b8d83df8073241a693bca35a1fd8
xiao zhaoxia <xiaozhaoxia@xiaodeMacBook-Pro.local>
2019-05-31 11:35:03 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -10,3 +10,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//  整个项目的入口文件
